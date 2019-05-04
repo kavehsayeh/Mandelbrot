d5c56266e90721baa6c3305ff973c172b916c42e
kavehsayeh <kavehsayeh@gmail.com>
2019-05-04 09:47:58 -0700

    Support for burning ship fractal added

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,8 +3,9 @@ from PIL import Image, ImageDraw
 
 def mandelbrot(c, maxIterations):
     """
-    The generating function to be used for the fractal. Returns the number of 
-    iterations it takes for the value to escape.
+    The generating function to be used for the Mandelbrot set. Returns the 
+    number of iterations it takes for the value to escape, or the maximum 
+    number of iterations.
     """
     z = 0
     i = 0
@@ -15,21 +16,35 @@ def mandelbrot(c, maxIterations):
         i += 1
     return i
 
+def burning_ship(c, maxIterations):
+    """
+    The generating function to be used for the burning ship fractal. Returns 
+    the number of iterations it takes for the value to escape, or the maximum 
+    number of iterations.
+    """
+    z = 0
+    i = 0
+    while i < maxIterations:
+        z = (complex(abs(z.real), abs(z.imag)))**2 + c
+        if abs(z) > 2:
+            break
+        i += 1
+    return i
 
-def plot_fractal(res, bottom_left, top_right, maxIter):
+def plot_fractal(res, bottom_left, top_right, maxIter, gen_func):
     """
     Plots the fractal using PIL. res specifies the number of pixels per one 
     unit. bottom_left and top_right are complex numbers specifying the corners 
-    of the image.
+    of the image. maxIter is the number of iterations to run the generating
+    function.
     """
     dim = (int(res * (top_right.real - bottom_left.real)),
            int(res * (top_right.imag - bottom_left.imag)))
-    # im = Image.new('RGBA', dim)
 
-    xvalues = numpy.linspace(bottom_left.real, top_right.real, num=dim[0], retstep=True, 
-                             endpoint=False)
-    yvalues = numpy.linspace(bottom_left.imag, top_right.imag, num=dim[1], retstep=True, 
-                             endpoint=False)
+    xvalues = numpy.linspace(bottom_left.real, top_right.real, num=dim[0], 
+    						 retstep=True, endpoint=False)
+    yvalues = numpy.linspace(bottom_left.imag, top_right.imag, num=dim[1], 
+    						 retstep=True, endpoint=False)
 
     xvalues_real = xvalues[0]
     xvalues_image = list(range(dim[0]))
@@ -41,13 +56,13 @@ def plot_fractal(res, bottom_left, top_right, maxIter):
     points_image = [(x, y) for x in xvalues_image for y in yvalues_image]
     points = zip(points_real, points_image)
 
-    mask = Image.new('RGB', dim, (0,0,0)) # creates a black base image
+    mask = Image.new('L', dim, 0) # creates a black base image
     d = ImageDraw.Draw(mask)
 
     for z in points:
-        if mandelbrot(z[0], maxIter) != maxIter:
-            d.point(z[1], fill=(255, 255, 255))
-            # if the point is not in the mandelbrot set, color it white
+        if gen_func(z[0], maxIter) != maxIter:
+            d.point(z[1], fill=255)
+            # if the point is not in the fractal, color it white
     mask.save(r"fractal.tif")
 
 
@@ -63,4 +78,4 @@ if __name__ == '__main__':
     c2 = complex(c2_r, c2_i)
     
     iterations = int(input("Iterations: "))
-    plot_fractal(resolution, c1, c2, iterations)
+    plot_fractal(resolution, c1, c2, iterations, mandelbrot)
